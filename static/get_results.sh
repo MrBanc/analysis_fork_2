cf787168f7cb21be108a6228982662b89c8afa89
student <gaulthier.gain@uliege.be>
2022-05-10 17:01:12 +0200

    update

--- static/get_results.sh
+++ static/get_results.sh
@@ -3,7 +3,7 @@ CSV="data.csv"
 BIN="../bin/"
 declare -a array=("haproxy" "lighttpd" "memcached" "nginx" "redis-server" "sqlite3" "weborf")
 
-echo "app;#syscalls(static)" > $CSV
+echo "app,#syscalls(static)" > $CSV
 for k in "${array[@]}"
 do
     python3 static_analyser.py --app "${BIN}${k}" --csv True --display False --verbose False >> $CSV
