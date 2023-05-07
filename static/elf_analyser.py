afe13b36ef57d5d659c1b183ce33c63e3bdf7a40
Knott Benoît <benoitknott060@gmail.com>
2023-05-07 17:12:07 +0200

    check binary is x86_64

--- static/elf_analyser.py
+++ static/elf_analyser.py
@@ -14,7 +14,8 @@ PLT_SECTION  = ".plt"
 def is_valid_binary(binary):
     return (binary is not None
             and binary.format == lief.EXE_FORMATS.ELF
-            and binary.header.identity_class == lief.ELF.ELF_CLASS.CLASS64)
+            and binary.header.identity_class == lief.ELF.ELF_CLASS.CLASS64
+            and binary.header.machine_type == lief.ELF.ARCH(62))
 
 def get_syscalls_from_symbols(binary, syscalls_set):
     for sect_it in [binary.dynamic_symbols, binary.static_symbols,
